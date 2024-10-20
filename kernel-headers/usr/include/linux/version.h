d55b236d9e6750139a1b3f15149837d1f87b7bd2
kondors1995 <normandija1945@gmail.com>
2024-10-20 16:12:07 +0000

    raphael-kernel: update to @SOVIET-ANDROID/kernel_xiaomi_raphael/1d23f9f

--- kernel-headers/usr/include/linux/version.h
+++ kernel-headers/usr/include/linux/version.h
@@ -2,4 +2,4 @@
 #define KERNEL_VERSION(a,b,c) (((a) << 16) + ((b) << 8) + ((c) > 255 ? 255 : (c)))
 #define LINUX_VERSION_MAJOR 4
 #define LINUX_VERSION_PATCHLEVEL 14
-#define LINUX_VERSION_SUBLEVEL 352
+#define LINUX_VERSION_SUBLEVEL 353
